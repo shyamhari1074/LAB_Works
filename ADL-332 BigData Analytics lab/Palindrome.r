bcf7a97ecf8b34cff191a172fc3532d4dcdb6d4c
Shyam Hari <shyamhwrk@gmail.com>
2026-03-10 21:11:53 +0530

    Update Palindrome.r

--- ADL-332 BigData Analytics lab/Palindrome.r
+++ ADL-332 BigData Analytics lab/Palindrome.r
@@ -18,3 +18,20 @@ is_palindrome <- function(word) {
   return(word == reversed)
 }
 is_palindrome("madam")
+
+
+//// 
+word <- readline("Enter a word: ")
+flag <- TRUE
+n <- nchar(word)
+for (i in 1:(n/2)) {
+  if (substr(word, i, i) != substr(word, n - i + 1, n - i + 1)) {
+    flag <- FALSE
+    break
+  }
+}
+if (flag) {
+  print("Word Palindrome")
+} else {
+  print("Not a palindrome")
+}
